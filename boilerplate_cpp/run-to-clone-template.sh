e90f3b57f5c63867cff6ba4da83d88a31debc2b5
nkarl <21993921+nkarl@users.noreply.github.com>
2021-11-16 11:23:56 -0800

    fixed clone shell script

--- boilerplate_cpp/run-to-clone-template.sh
+++ boilerplate_cpp/run-to-clone-template.sh
@@ -2,7 +2,7 @@
 ## by cloning from a seed template.
 #!/bin/zsh
 
-function generate_template_cpp() {
+generate_template_cpp () {
 	local TEMPLATE_PATH="$HOME/.custom-scripts/boilerplate_cpp/template"
     local WORKSPACE_NAME=${1:-template-cpp}
 
@@ -13,7 +13,25 @@ function generate_template_cpp() {
     	else echo "ERROR: WORKSPACE ALREADY EXISTS."
     	fi
 
+        temp=$(pwd)
+        echo $temp
     	## Navigate into the new workspace
     	cd "$WORKSPACE_NAME"
 }
 alias newcpp=generate_template_cpp
+
+#function generate_template_cpp() {
+	#local TEMPLATE_PATH="$HOME/.custom-scripts/boilerplate_cpp/template"
+    #local WORKSPACE_NAME=${1:-template-cpp}
+
+	### If workspace NOT already exists, create it:
+	#if [ ! -d "$WORKSPACE_NAME" ]; then
+		#mkdir "$WORKSPACE_NAME"
+		#cp -r $TEMPLATE_PATH/. $WORKSPACE_NAME/
+        #else echo "ERROR: WORKSPACE ALREADY EXISTS."
+        #fi
+
+        ### Navigate into the new workspace
+        #cd "$WORKSPACE_NAME"
+#}
+#alias newcpp=generate_template_cpp
